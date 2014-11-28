c99475472aa9c643a4b12e82e71da97084c6a2bb
yap cheah shen <yapcheahshen@gmail.com>
2014-11-28 17:57:03 +0800

    first build

---
+++ components/testapp-main/index.jsx
@@ -0,0 +1,33 @@
+var require_kdb=[{ filename:"testapp.kdb"  , url:"http://rawgit.com/ksanaforge/testapp/master/testapp.kdb" , desc:""}];  
+var Fileinstaller=Require("fileinstaller");
+var kde=Require("ksana-document").kde;
+var kse=Require("ksana-document").kse;
+var bootstrap=Require("bootstrap");
+var main = React.createClass({
+  getInitialState:function(){
+    return {};
+  },
+  onReady:function(usage,quota) {
+    if (!this.state.db) kde.open("testapp",function(db){
+        this.setState({db:db});  
+    },this);      
+    this.setState({quota:quota,usage:usage});
+  },
+  openFileinstaller:function(autoclose) {
+    if (window.location.origin.indexOf("http://127.0.0.1")==0) {
+      require_kdb[0].url=window.location.origin+window.location.pathname+"testapp.kdb";
+    }
+    return <Fileinstaller quota="512M" autoclose={autoclose} needed={require_kdb} onReady={this.onReady}/>
+  },
+  render: function() {
+    if (!this.state.quota) {
+        return this.openFileinstaller(true);
+    } else { 
+    return (
+      <div className="main">
+		{"Main of testapp"}      </div>
+      );
+    }
+  }
+});
+module.exports=main;
---
+++ ksana.js
@@ -0,0 +1,30 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "testapp",
+ "date": "2014-11-28T09:56:45.646Z",
+ "minruntime": "1.3",
+ "baseurl": "http://rawgit.com/ksanaforge/testapp/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "build.js",
+  "build.css",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "testapp.kdb"
+ ],
+ "filesizes": [
+  422,
+  277980,
+  644338,
+  335
+ ],
+ "filedates": [
+  "2014-11-28T09:56:38.000Z",
+  "2014-11-28T09:56:38.000Z",
+  "2014-11-28T09:56:38.000Z",
+  "2014-11-28T09:56:38.000Z"
+ ]
+})
